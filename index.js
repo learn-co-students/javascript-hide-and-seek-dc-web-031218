9d8e40082136211d3e46f49e3fb1a9437e0c774b
Austin Wolfe <wolfe_austin@yahoo.com>
2018-04-30 17:26:02 -0400

    Done.

--- index.js
+++ index.js
@@ -11,13 +11,17 @@ function deepestChild(){
   var lis=document.getElementById("grand-node").querySelectorAll("div");
   var test;
   for(let i=0;i<lis.length-1;i++) {
+    //each iteration we are replacing what we have in test
+    //on the last iteration, we have the deepest child 
     test=lis[i].querySelector("div");
   }
 return test;
 }
 
 function increaseRankBy(n){
+  //getting all of the li elements
   var lis=document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  //iterating through them and incrementing them by n
   for(let i=0;i<lis.length;i++) {
     lis[i].innerHTML=parseInt(lis[i].innerHTML)+n;
   }
